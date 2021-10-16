c3d8e41c7235822b7761cbed0d60f271b64bcca3
ashiajoseph <ashiajoseph@gmail.com>
2021-10-16 15:56:03 +0530

    Added progress to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,10 +5,11 @@ class TasksController < ApplicationController
   after_action :verify_policy_scoped, only: :index
   before_action :authenticate_user_using_x_auth_token, except: [:new, :edit]
   before_action :load_task, only: %i[show update destroy]
-
+  before_action :ensure_authorized_update_to_restricted_attrs, only: %i[update]
   def index
     tasks = policy_scope(Task)
-    render status: :ok, json: { tasks: tasks }
+    @pending_tasks = tasks.pending.as_json(include: { assigned_user: { only: %i[name id] } })
+    @completed_tasks = tasks.completed
   end
 
   def create
@@ -48,13 +49,21 @@ class TasksController < ApplicationController
   private
 
     def task_params
-      params.require(:task).permit(:title, :assigned_user_id)
+      params.require(:task).permit(:title, :assigned_user_id, :progress)
     end
 
     def load_task
       @task = Task.find_by(slug: params[:slug])
       unless @task
         render status: :not_found, json: { error: t("task.not_found") }
+      end
+    end
+
+    def ensure_authorized_update_to_restricted_attrs
+      is_editing_restricted_params = Task::RESTRICTED_ATTRIBUTES.any? { |a| task_params.key?(a) }
+      is_not_owner = @task.task_owner_id != @current_user.id
+      if is_editing_restricted_params && is_not_owner
+        handle_authorization_error
       end
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  RESTRICTED_ATTRIBUTES = %i[title assigned_user_id]
+
+  enum progress: { pending: 0, completed: 1 }
   belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
   belongs_to :task_owner, foreign_key: "task_owner_id", class_name: "User"
   has_many :comments, dependent: :destroy
