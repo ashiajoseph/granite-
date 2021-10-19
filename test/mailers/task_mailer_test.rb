603ca30f387806484a6cc2f70bcbb3e552e5c6fb
ashiajoseph <ashiajoseph@gmail.com>
2021-10-19 09:17:45 +0530

    Added test cases for sidekiq email sending feature

--- test/mailers/task_mailer_test.rb
+++ test/mailers/task_mailer_test.rb
@@ -14,4 +14,15 @@ class TaskMailerTest < ActionMailer::TestCase
     assert_equal [@user.email], email.to
     assert_equal "Pending Tasks", email.subject
   end
+  def test_task_mailer_after_action_create_user_notifications
+    assert_equal 0, UserNotification.count
+    TaskMailer.pending_tasks_email(@user.id).deliver_now
+
+    assert_equal 1, UserNotification.count
+  end
+  def test_task_mailer_shouldnt_deliver_email_if_receiver_is_not_present
+    email = TaskMailer.pending_tasks_email("").deliver_now
+    assert ActionMailer::Base.deliveries.empty?
+    assert_nil email
+  end
 end
